1ade28cd933ce9660d4f5ee0e6f2f42c9abfb79f
Maksym Shpak <maksym.shpak@additiv.com>
2024-05-22 10:56:58 +0300

    feat(di): created di package
    Added a fake di package to test JSR flow

---
+++ src/packages/core/di/di.ts
@@ -0,0 +1,11 @@
+/**
+ * Returns true if the number is odd, false otherwise.
+ * @param num The number to check.
+ * @returns True if the number is odd, false otherwise.
+ * @example
+ * isOdd(3); // true
+ * isOdd(4); // false
+ */
+export function isOdd(num: number): boolean {
+    return num % 2 !== 0;
+  }
